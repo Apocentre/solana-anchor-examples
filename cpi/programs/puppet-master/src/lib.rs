38d15c481543e3f67f3f447c13e7d907f6847c76
ppoliani <ppoliani@gmail.com>
2021-12-23 10:19:29 +0000

    feat(*): use native invoke_signed

--- cpi/programs/puppet-master/src/lib.rs
+++ cpi/programs/puppet-master/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use solana_program::{
+  instruction::{Instruction},
+  program::{invoke_signed},
+};
 // load the relevant data from the puppet program
 use puppet::cpi::accounts::{SetData, SetDataAuth};
 use puppet::program::Puppet;
@@ -24,19 +28,35 @@ pub mod puppet_master {
     let puppet_master_pda = ctx.accounts.puppet_master_pda.to_account_info();
     let seeds: &[&[u8]] = &[b"puppet_master"];
     let signer_seeds = &[&seeds[..]];
+    let (_, bump_seed) = Pubkey::find_program_address(seeds, ctx.program_id);
 
     let cpi_accounts = SetDataAuth {
       puppet: ctx.accounts.puppet.to_account_info(),
-      authority: puppet_master_pda,
+      authority: puppet_master_pda.clone(),
     };
 
     let cpi_ctx = CpiContext::new_with_signer(
-      cpi_program,
+      cpi_program.clone(),
       cpi_accounts,
       signer_seeds
     );
+
+    let ix = Instruction {
+      program_id: *cpi_program.key,
+      accounts: cpi_ctx.to_account_metas(Some(true)),
+      data: 10_u64.to_be_bytes().to_vec()
+    };
+
+    invoke_signed(
+      &ix,
+      &[
+        ctx.accounts.puppet.to_account_info(),
+        puppet_master_pda,
+      ],
+      &[seeds, &[&[bump_seed]]]
+    )
     
-    puppet::cpi::set_data_auth(cpi_ctx, data)
+    // puppet::cpi::set_data_auth(cpi_ctx, data)
   }
 }
 
