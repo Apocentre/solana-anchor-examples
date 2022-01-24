139614fd34473c9b9ffa626ac7ae5b33bf81d238
ppoliani <ppoliani@gmail.com>
2022-01-24 14:48:24 +0000

    feat(sig-verify): implement basic contribute method

--- cpi/programs/puppet-master/src/lib.rs
+++ cpi/programs/puppet-master/src/lib.rs
@@ -43,7 +43,7 @@ pub mod puppet_master {
       signer_seeds
     );
 
-    // The following verbose way is what the puppet::cpi::set_data_auth will fo under the hood 
+    // The following verbose way is what the puppet::cpi::set_data_auth will do under the hood 
     //
     // let ix_data = (puppet::instruction::SetDataAuth{data}).data();
 
--- sig-verify/programs/sig-verify/src/lib.rs
+++ sig-verify/programs/sig-verify/src/lib.rs
@@ -1,13 +1,41 @@
 use anchor_lang::prelude::*;
+use borsh::BorshSerialize;
+use sodalite::sign_attached_open;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod sig_verify {
-    use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
+  use super::*;
+  pub fn initialize(ctx: Context<Initialize>, authority: Pubkey) -> ProgramResult {
+    let state = &mut ctx.accounts.state;
+    state.authority = authority;
+
+    Ok(())
+  }
+
+  pub fn contribute(ctx: Context<Contribute>, data: ContributeData) -> ProgramResult {
+    let state = &mut ctx.accounts.state;
+    let raw_message = ContributeMsg {
+      program: ctx.program_id,
+      nonce: data.nonce,
+      sender: ctx.accounts.sender.unsigned_key(),
+      amount: data.amount
+    };
+
+    let mut message: Vec<u8> = Vec::new();
+    raw_message.serialize(&mut message).unwrap();
+
+    if let Err(_) = sign_attached_open(
+      &mut message,
+      &data.sig,
+      &state.authority.to_bytes()
+    ) {
+      return Err(ErrorCode::InvalidSig.into())
     }
+
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
@@ -24,7 +52,36 @@ pub struct Initialize<'info> {
   pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Contribute<'info> {
+  #[account(mut)]
+  pub state: Account<'info, State>,
+  #[account(mut)]
+  pub sender: Signer<'info>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ContributeMsg<'info> {
+  program: &'info Pubkey,
+  nonce: [u8; 32],
+  sender: &'info Pubkey,
+  amount: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ContributeData {
+  nonce: [u8; 32],
+  sig: [u8; 64],
+  amount: u64,
+}
+
 #[account]
 pub struct State {
-  authority: PubKey,
+  authority: Pubkey,
+}
+
+#[error]
+pub enum ErrorCode {
+  #[msg("invalid sig")]
+  InvalidSig,
 }
