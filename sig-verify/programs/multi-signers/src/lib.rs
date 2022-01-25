a243fb8c54ec3ea329074b99f196da9a81cabcaf
ppoliani <ppoliani@gmail.com>
2022-01-25 09:37:53 +0000

    feat(*): implement the whitelist logic with mutli signer checks

---
+++ sig-verify/programs/multi-signers/src/lib.rs
@@ -0,0 +1,59 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod multi_signers {
+  use super::*;
+
+  pub fn initialize(ctx: Context<Initialize>, auth_provider: Pubkey) -> ProgramResult {
+    let state = &mut ctx.accounts.state;
+    state.auth_provider = auth_provider;
+
+    Ok(())
+  }
+
+  pub fn contribute(
+    ctx: Context<Contribute>,
+    amount: u64,
+  ) -> ProgramResult {
+    let state = &mut ctx.accounts.state;
+
+    // tx should be signed by both the sender and the auth provider
+    if ctx.accounts.auth_provider != state.auth_provider {
+      return Err(ErrorCode::Unauthorized.into())
+    }
+
+    // TODO: we know the user is authenticated thus we can continue with the business logic
+  }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+  #[account(
+    init,
+    payer = user,
+    space = 8 + 32
+  )]
+  pub state: Account<'info, State>,
+
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Contribute<'info> {
+  #[account(mut)]
+  pub state: Account<'info, State>,
+  #[account()]
+  pub sender: Signer<'info>,
+  #[account()]
+  pub auth_provider: Signer<'info>,
+}
+
+#[error]
+pub enum ErrorCode {
+  #[msg("unauthorized")]
+  Unauthorized,
+}
